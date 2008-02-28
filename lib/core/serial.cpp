72ca0a1a0f15e7f00993292dbc1114258dd86f80
Max Lapan <max.lapan@gmail.com>
2008-02-28 09:17:13 +0300

    Implemented serial port timeout.

--- lib/core/serial.cpp
+++ lib/core/serial.cpp
@@ -8,6 +8,9 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <unistd.h>
 
 
 // --------------------------------------------------
@@ -70,8 +73,26 @@ QByteArray RealSerialPort::receive (int timeout) throw (QString)
     // wait for sync byte
     char c, cc[4];
     QByteArray res;
+    struct timeval tv;
+    fd_set fds;
+    int ret;
 
+    tv.tv_sec = timeout;
+    tv.tv_usec = 0;
+
+    FD_ZERO (&fds);
+    FD_SET (_fd, &fds);
+
+    // wait for correct sequence
     while (1) {
+        ret = select (_fd+1, &fds, NULL, NULL, &tv);
+
+        // timeout
+        if (!ret)
+            throw QString ("Timeout reading serial port");
+        if (ret < 0)
+            throw QString ("Error occured while waiting for data");
+        
         if (read (_fd, &c, 1) < 0)
             throw QString ("Error receiving data %d").arg (errno);
         if (c == (char)0xAA)
