19a5e88549952fd67f7cb0120282fc0b898274de
Max Lapan <max.lapan@gmail.com>
2008-03-16 13:15:37 +0300

    Translated.

--- gui/gui_ru.ts
+++ gui/gui_ru.ts
@@ -2,134 +2,187 @@
 <!DOCTYPE TS><TS version="1.1" language="ru">
 <defaultcodec></defaultcodec>
 <context>
+    <name>Daemon</name>
+    <message>
+        <location filename="" line="0"/>
+        <source>Connection to hardware failed. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка соединения с контроллером. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Controller connected to hardware</source>
+        <translation>Соединение с контроллером установлено</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot set active stages. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка активизации этапов. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot get grain flow. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка получения расхода зерна. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot start auto mode. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка перехода в автоматический режим. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot stop auto mode. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка остановки автоматического режима. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot toggle auto mode. Reason: &apos;%1&apos;</source>
+        <translation>Ошибка приостановки автоматического режима. Ошибка: &apos;%1&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot start water at stage %1. Reason: &apos;%2&apos;</source>
+        <translation>Ошибка пуска воды в этапе %1. Ошибка: &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot stop water at stage %1. Reason: &apos;%2&apos;</source>
+        <translation>Ошибка остановки воды в этапе %1. Ошибка: &apos;%2&apos;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Expected number, got &apos;%1&apos;</source>
+        <translation>Ожидалось число, получено &apos;%1&apos;</translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Plaun Controller</source>
         <translation>Система управления увлажнением зерна &quot;Плаун&quot;</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Connect</source>
         <translation>Контакт</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Config</source>
         <translation>Конфигурация</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Check</source>
         <translation>Контроль</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Params</source>
         <translation>Параметры</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Sensors</source>
         <translation>Датчики</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Start</source>
         <translation>Старт</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stop</source>
         <translation>Стоп</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Pause</source>
         <translation>Пауза</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Clean</source>
         <translation>Промывка</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>History</source>
         <translation>Архив</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Date and time</source>
         <translation>Дата и время</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Active stages</source>
         <translation>Активные этапы</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stage 1</source>
         <translation>Этап 1</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stage 2</source>
         <translation>Этап 2</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stage 3</source>
         <translation>Этап 3</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stage 4</source>
         <translation>Этап 4</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>State</source>
         <translation>Состояние</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Water</source>
         <translation>Подача воды</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Grain sensors</source>
         <translation>Состояние ДНЗ</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>grain sensors enabled</source>
         <translation>ДНЗ включены</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>CheckBox</source>
         <translation>CheckBox</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>View</source>
         <translation>Вид</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>History page stub</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>&amp;Send</source>
         <translation>&amp;Выполнить</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Terminus&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
@@ -137,111 +190,209 @@ p, li { white-space: pre-wrap; }
         <translation>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;p, li { white-space: pre-wrap; }&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Terminus&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;Интерактивная консоль.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stages</source>
         <translation>Этапы</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Ctrl+S</source>
         <translation>Ctrl+S</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Ctrl+H</source>
         <translation>Ctrl+H</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Console</source>
         <translation>Консоль</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Ctrl+C</source>
         <translation>Ctrl+C</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Apply</source>
+        <translation>Установить</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Label</source>
+        <translation>Параметр</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Value</source>
+        <translation>Значение</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Refresh</source>
+        <translation>Обновить</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Send &amp;file</source>
+        <translation>&amp;Скрипт</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Verdana&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Terminus&apos;;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;p, li { white-space: pre-wrap; }&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Verdana&apos;; font-size:11pt; font-weight:400; font-style:normal;&quot;&gt;&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Terminus&apos;;&quot;&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Global</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Water pressure</source>
+        <translation>Давление воды</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Stage %1</source>
+        <translation>Этап %1</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Water flow</source>
+        <translation>Расход воды</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Grain flow</source>
+        <translation>Расход зерна</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Grain humidity</source>
+        <translation>Влажность зерна</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Grain temp.</source>
+        <translation>Температура зерна.</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Grain nature</source>
+        <translation>Натура зерна</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Water started on %1 stage</source>
+        <translation>Вода в этапе %1 запущена</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Water stopped on %1 stage</source>
+        <translation>Вода остановлена в этапе %1</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Open script</source>
+        <translation>Открыть скрипт</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Scripts (*.txt)</source>
+        <translation>Скрипт (*.txt)</translation>
+    </message>
+    <message>
+        <location filename="" line="0"/>
+        <source>Cannot open script file &apos;%1&apos; for reading.</source>
+        <translation>Не могу открыть файл со скриптом &apos;%1&apos;.</translation>
     </message>
 </context>
 <context>
     <name>StageControl</name>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Stage %1</source>
         <translation>Этап %1</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Sensor</source>
         <translation>Датчик</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Grain flow</source>
         <translation>Расход зерна</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Humidity</source>
         <translation>Влажность</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Nature</source>
         <translation>Натура</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Temperature</source>
         <translation>Температура</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>%1 t/h</source>
         <translation>%1 т/ч</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>%1 %</source>
         <translation>%1 %</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>%1 g/l</source>
         <translation>%1 г/л</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>%1 C</source>
         <translation>%1 °C</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Water gate</source>
         <translation>Регулятор воды</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Water
 gate</source>
         <translation type="obsolete">Регулятор воды</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Water flow</source>
         <translation>Расход воды</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>%1 l/h</source>
         <translation>%1 л/ч</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>BSU</source>
         <translation>БШУ</translation>
     </message>
     <message>
-        <location filename="" line="7471221"/>
+        <location filename="" line="0"/>
         <source>Given grain humidity</source>
         <translation>Заданная влажность зерна</translation>
     </message>
