b20f7b8c16ae534e2a25dbdf8ab5dcf2abb15f97
Max Lapan <max.lapan@gmail.com>
2008-04-10 02:19:48 +0400

    Drawn some lines in filter block control.

--- gui/bsucontrol.cpp
+++ gui/bsucontrol.cpp
@@ -17,19 +17,28 @@ void BSUControl::paintEvent (QPaintEvent* event)
 {
     QPainter p (this);
     QRect r;
+    QPoint pt;
 
     p.setFont (QFont ("Arial", 15));
     p.setPen (Qt::black);
     
-    p.drawText (QRect (0, 0, geometry ().width (), 20), Qt::AlignHCenter, tr ("BSU"));
+    p.drawText (QRect (0, 30, geometry ().width (), 20), Qt::AlignHCenter, tr ("Filter block"));
 
-    r.setRect (20, 50, geometry ().width () - 40, geometry ().height () - 60);
+    r.setRect (20, 20, geometry ().width ()-40, geometry ().height ()-40);
     p.drawRect (r);
 
     p.setFont (QFont ("Arial", 12));
 
-    r.setRect (30, 50 + (geometry ().height () - 60) / 2, geometry ().width () / 2 - 30,  20);
+    r.setRect (30, geometry ().height ()/2, geometry ().width ()/2 - 30, 20);
 
     p.drawText (r, Qt::AlignLeft, tr ("Pressure"));
-    p.drawText (r.translated (geometry ().width () / 2 - 30, 0), Qt::AlignLeft, tr ("%1 bar").arg (QString ().sprintf ("%.2f", _waterPressure)));
+    p.drawText (r.translated (geometry ().width ()/2-30, 0), Qt::AlignLeft, tr ("%1 bar").arg (QString ().sprintf ("%.2f", _waterPressure)));
+
+    pt = QPoint (0, geometry ().height ()/2 - 30);
+    p.drawLine (pt, pt + QPoint (20, 0));
+    p.drawLine (pt + QPoint (0, 60), pt + QPoint (20, 60));
+
+    pt = QPoint (geometry ().width ()-20, geometry ().height ()/2 - 30);
+    p.drawLine (pt, pt + QPoint (20, 0));
+    p.drawLine (pt + QPoint (0, 60), pt + QPoint (20, 60));
 }
