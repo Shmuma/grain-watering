47926866862c1fc1e55518b0bd9f13942516716d
Max Lapan <max.lapan@gmail.com>
2008-07-09 02:22:09 +0400

    Stage control's target humidity editor won't overwrite when has focus.

--- gui/stagecontrol.cpp
+++ gui/stagecontrol.cpp
@@ -403,7 +403,7 @@ void StageControl::setTargetWaterFlow (double val)
     _targetWaterFlow = val;
     update ();
 
-    if (_mode == StageSettings::M_SemiAuto)
+    if (_mode == StageSettings::M_SemiAuto && !_humidityEdit->hasFocus ())
         _humidityEdit->setText (QString ().sprintf ("%.0f", val));
 }
 
